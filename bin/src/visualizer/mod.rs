e63a7e27a7d129dcaeb5c882406ad526f964398e
Marco <marcotoniolomail@gmail.com>
2023-11-30 15:40:29 +0100

    changed to rgbimage

--- bin/src/visualizer/mod.rs
+++ bin/src/visualizer/mod.rs
@@ -1,5 +1,4 @@
 use image::{Rgb, RgbImage};
-use image::DynamicImage;
 use image::ImageFormat;
 use imageproc::rect::Rect;
 
@@ -7,12 +6,11 @@ use std::sync::{Arc, Mutex};
 use rayon::prelude::*;
 use robotics_lib::world::tile::*;
 
-fn create_image_from_tiles(tiles: &[Vec<Tile>], bot_position: (usize, usize)) -> RgbImage {
-    const TILE_SIZE: u32 = 10;
-    let width: u32 = tiles[0].len() as u32 * TILE_SIZE;
-    let height: u32 = tiles.len() as u32 * TILE_SIZE;
+fn create_image_from_tiles(tiles: &[Vec<Tile>], bot_position: (usize, usize), tile_size: usize) -> RgbImage {
+    let width = tiles[0].len() * tile_size;
+    let height = tiles.len() * tile_size;
 
-    let img = Arc::new(Mutex::new(RgbImage::new(width, height)));
+    let img = Arc::new(Mutex::new(RgbImage::new(width as u32, height as u32)));
 
     const COLOR_DEEP_WATER: Rgb<u8> = Rgb([5, 25, 90]);         // DeepWater (Blu scuro)
     const COLOR_SHALLOW_WATER: Rgb<u8> = Rgb([45, 100, 160]);   // ShallowWater (Blu chiaro)
@@ -43,8 +41,8 @@ fn create_image_from_tiles(tiles: &[Vec<Tile>], bot_position: (usize, usize)) ->
             };
 
             // Inside the inner loop
-            let rect = Rect::at(x as i32 * TILE_SIZE as i32, y as i32 * TILE_SIZE as i32)
-                .of_size(TILE_SIZE, TILE_SIZE);
+            let rect = Rect::at(x as i32 * tile_size as i32, y as i32 * tile_size as i32)
+                .of_size(tile_size as u32, tile_size as u32);
 
             // Lock the Mutex to get a mutable reference to the image
             let mut img_ref = img.lock().unwrap();
@@ -58,11 +56,11 @@ fn create_image_from_tiles(tiles: &[Vec<Tile>], bot_position: (usize, usize)) ->
 
 
 
-    for dy in 0..TILE_SIZE {
-        for dx in 0..TILE_SIZE {
+    for dy in 0..tile_size {
+        for dx in 0..tile_size {
             img.lock().unwrap().put_pixel(
-                bot_x as u32 * TILE_SIZE + dx,
-                bot_y as u32 * TILE_SIZE + dy,
+                (bot_x * tile_size + dx) as u32,
+                (bot_y * tile_size + dy) as u32,
                 bot_color,
             );
         }
@@ -77,7 +75,7 @@ fn create_image_from_tiles(tiles: &[Vec<Tile>], bot_position: (usize, usize)) ->
 }
 
 pub fn save_world_image(tiles: &[Vec<Tile>], bot_position: (usize, usize), file_name: &str) {
-    let img: DynamicImage = create_image_from_tiles(tiles, bot_position).into();
+    let img = create_image_from_tiles(tiles, bot_position, 10);
 
     if let Err(e) = img.save_with_format(file_name, ImageFormat::Png) {
         println!("Error saving the image: {}", e);
