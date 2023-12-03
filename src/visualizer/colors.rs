a749d2d5f6d1ca4f0231038ac6fcd56574e65fa1
FrostWalk <federico.frigerio@studenti.unitn.it>
2023-12-03 21:16:48 +0100

    Macro for colors

--- src/visualizer/colors.rs
+++ src/visualizer/colors.rs
@@ -1,26 +1,35 @@
-use image::Rgb;
+pub(crate) mod tile {
+    use image::Rgb;
 
-/// DeepWater color (deep blue)
-pub(crate) const DEEP_WATER: Rgb<u8> = Rgb([5, 25, 90]);
-/// ShallowWater color (Dolce & Gabbana light blue)
-pub(crate) const SHALLOW_WATER: Rgb<u8> = Rgb([45, 100, 160]);
-/// Sand color (plaid yellow)
-pub(crate) const SAND: Rgb<u8> = Rgb([240, 230, 140]);
-/// Grass color (minecraft plain grass green)
-pub(crate) const GRASS: Rgb<u8> = Rgb([74, 111, 40]);
-/// Street color (dark grey)
-pub(crate) const STREET: Rgb<u8> = Rgb([90, 90, 90]);
-/// Hill color (minecraft dirt brown)
-pub(crate) const HILL: Rgb<u8> = Rgb([146, 104, 41]);
-/// Mountain color (minecraft stone grey)
-pub(crate) const MOUNTAIN: Rgb<u8> = Rgb([160, 160, 160]);
-/// Snow color (off white)
-pub(crate) const SNOW: Rgb<u8> = Rgb([250, 249, 246]);
-/// Lava color (minecraft lava orange)
-pub(crate) const LAVA: Rgb<u8> = Rgb([255, 129, 0]);
-/// Gray color (straight grey)
-pub(crate) const GRAY: Rgb<u8> = Rgb([128, 128, 128]);
-/// Black color (black)
-pub(crate) const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
-/// Garbage color (solid yellow)
-pub(crate) const GARBAGE: Rgb<u8> = Rgb([255, 232, 28]);
+    /// DeepWater color (deep blue)
+    pub(crate) const DEEP_WATER: Rgb<u8> = Rgb([5, 25, 90]);
+    /// ShallowWater color (Dolce & Gabbana light blue)
+    pub(crate) const SHALLOW_WATER: Rgb<u8> = Rgb([45, 100, 160]);
+    /// Sand color (plaid yellow)
+    pub(crate) const SAND: Rgb<u8> = Rgb([240, 230, 140]);
+    /// Grass color (Minecraft plain grass green)
+    pub(crate) const GRASS: Rgb<u8> = Rgb([74, 111, 40]);
+    /// Street color (dark grey)
+    pub(crate) const STREET: Rgb<u8> = Rgb([90, 90, 90]);
+    /// Hill color (light soil brown)
+    pub(crate) const HILL: Rgb<u8> = Rgb([146, 104, 41]);
+    /// Mountain color (Minecraft stone grey)
+    pub(crate) const MOUNTAIN: Rgb<u8> = Rgb([160, 160, 160]);
+    /// Snow color (off white)
+    pub(crate) const SNOW: Rgb<u8> = Rgb([250, 249, 246]);
+    /// Lava color (Minecraft lava orange)
+    pub(crate) const LAVA: Rgb<u8> = Rgb([255, 129, 0]);
+    /// Gray color (straight grey)
+    pub(crate) const GRAY: Rgb<u8> = Rgb([128, 128, 128]);
+    /// Black color (black)
+    pub(crate) const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
+    /// Brick color (brick red)
+    pub(crate) const BRICK: Rgb<u8> = Rgb([188, 74, 60]);
+}
+
+pub(crate) mod content {
+    use image::Rgb;
+
+    /// Garbage color (solid yellow)
+    pub(crate) const GARBAGE: Rgb<u8> = Rgb([255, 232, 28]);
+}
--- src/visualizer/mod.rs
+++ src/visualizer/mod.rs
@@ -2,6 +2,24 @@ use image::{ImageFormat, RgbImage};
 use robotics_lib::world::tile::*;
 
 mod colors;
+/// Associates each tile with its color
+macro_rules! get_tile_color {
+    ($t:expr) => {
+        match $t {
+            TileType::DeepWater => colors::tile::DEEP_WATER,
+            TileType::ShallowWater => colors::tile::SHALLOW_WATER,
+            TileType::Sand => colors::tile::SAND,
+            TileType::Grass => colors::tile::GRASS,
+            TileType::Street => colors::tile::STREET,
+            TileType::Hill => colors::tile::HILL,
+            TileType::Mountain => colors::tile::MOUNTAIN,
+            TileType::Snow => colors::tile::SNOW,
+            TileType::Lava => colors::tile::LAVA,
+            TileType::Wall => colors::tile::BRICK,
+            _ => colors::tile::BLACK,
+        }
+    }
+}
 
 fn create_image_from_tiles(tiles: &[Vec<Tile>], _bot_position: (usize, usize), tile_size: usize) -> RgbImage {
     let size: u32 = (tile_size * tiles.len()) as u32;
@@ -9,35 +27,23 @@ fn create_image_from_tiles(tiles: &[Vec<Tile>], _bot_position: (usize, usize), t
 
     for (y, col) in tiles.iter().enumerate() {
         for (x, tile) in col.iter().enumerate() {
-            let color = match tile.tile_type {
-                TileType::DeepWater => colors::DEEP_WATER,
-                TileType::ShallowWater => colors::SHALLOW_WATER,
-                TileType::Sand => colors::SAND,
-                TileType::Grass => colors::GRASS,
-                TileType::Street => colors::STREET,
-                TileType::Hill => colors::HILL,
-                TileType::Mountain => colors::MOUNTAIN,
-                TileType::Snow => colors::SNOW,
-                TileType::Lava => colors::LAVA,
-                _ => colors::BLACK,
-            };
+            let color = get_tile_color!(tile.tile_type);
             img.put_pixel((x * tile_size) as u32, (y * tile_size) as u32, color);
         }
     }
-
     // for y in 0..tiles.len() {
     //     for x in 0..tiles[y].len() {
     //         let color = match tiles[y][x].tile_type {
-    //             TileType::DeepWater => colors::DEEP_WATER,
-    //             TileType::ShallowWater => colors::SHALLOW_WATER,
-    //             TileType::Sand => colors::SAND,
-    //             TileType::Grass => colors::GRASS,
-    //             TileType::Street => colors::STREET,
-    //             TileType::Hill => colors::HILL,
-    //             TileType::Mountain => colors::MOUNTAIN,
-    //             TileType::Snow => colors::SNOW,
-    //             TileType::Lava => colors::LAVA,
-    //             _ => colors::BLACK,
+    //             TileType::DeepWater => colors::tile::DEEP_WATER,
+    //             TileType::ShallowWater => colors::tile::SHALLOW_WATER,
+    //             TileType::Sand => colors::tile::SAND,
+    //             TileType::Grass => colors::tile::GRASS,
+    //             TileType::Street => colors::tile::STREET,
+    //             TileType::Hill => colors::tile::HILL,
+    //             TileType::Mountain => colors::tile::MOUNTAIN,
+    //             TileType::Snow => colors::tile::SNOW,
+    //             TileType::Lava => colors::tile::LAVA,
+    //             _ => colors::tile::BLACK,
     //         };
     //
     //         img.put_pixel((x * tile_size) as u32, (y * tile_size) as u32, color);
